cbc90074eb177e5c4080daa250eb05d914a86f70
rdnk2004 <115457357+rdnk2004@users.noreply.github.com>
2022-10-12 11:36:36 +0530

    Add files via upload

---
+++ Assignment4.py
@@ -0,0 +1,11 @@
+#define a function called square_of_number
+def square_of_number(n):
+    if n%2==0:
+        print("1")
+    else:
+        print("0")
+    
+n=int(input("Enter a number:"))
+square_of_number(n)
+
+
